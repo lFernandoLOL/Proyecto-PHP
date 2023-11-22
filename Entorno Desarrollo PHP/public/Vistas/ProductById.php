5268c508fa4b287775471a95c446668fd81cb0e4
Fernando <114144297+lFernandoLOL@users.noreply.github.com>
2023-11-22 17:41:24 +0100

    css

--- Entorno Desarrollo PHP/public/Vistas/ProductById.php
+++ Entorno Desarrollo PHP/public/Vistas/ProductById.php
@@ -4,98 +4,87 @@
 ?>
 
 <html>
-    <head>
-        <style>
-            .tabla {
-                border-collapse: collapse;
-                margin-bottom: 30px;
-                margin-top: 30px;
-                margin-left: auto;
-                margin-right: auto;
-            }
+<head>
+    <style>
+        .tabla {
+            border-collapse: collapse;
+            margin: 30px auto;
+        }
 
-            .tabla th, .tabla td {
-                border: 1px solid black;
-                padding: 8px;
-            }
+        .tabla th, .tabla td {
+            border: 1px solid black;
+            padding: 8px;
+        }
 
-            .tabla th {
-                background-color: lightgrey;
-                color: black;
-            }
+        .tabla th {
+            background-color: lightgrey;
+            color: black;
+        }
 
-            .tabla img {
-                max-width: 100px;
-                max-height: 100px;
-            }
+        .tabla img {
+            max-width: 100px;
+            max-height: 100px;
+        }
 
-            .back-button {
-                margin-top: 20px;
-                background-color: #808080; /* Cambiado a gris */
-                color: white;
-                padding: 10px 20px;
-                font-size: 16px;
-                text-decoration: none;
-                display: inline-block;
-                border-radius: 5px;
-                margin-bottom: 30px;
-            }
+        .back-button, .boton-agregar {
+            background-color: #808080; 
+            color: white;
+            padding: 10px 20px;
+            font-size: 16px;
+            text-decoration: none;
+            display: inline-block;
+            border-radius: 5px;
+            margin-bottom: 30px;
+        }
 
-            /* Añadido para hacer el hueco del nombre un poco más ancho */
-            .tabla td.nombre {
-                width: 150px;
-            }
+        .back-button {
+            margin-top: 20px;
+        }
 
-            /* Añadido para alinear el botón "Agregar" a la derecha */
-            .boton-agregar {
-                float: right;
-                background-color: #4CAF50; /* Verde */
-                color: white;
-                padding: 10px 20px;
-                font-size: 16px;
-                text-decoration: none;
-                display: inline-block;
-                border-radius: 5px;
-                margin-top: 20px;
-                margin-bottom: 30px;
-                margin-left: 10px; /* Espaciado entre los botones */
-            }
-        </style>
-    </head>
-    <body>
-        <div class='container'>
-            <h1 style='color:black; margin-top: 20px;'>Más información del producto seleccionado</h1>
+        .boton-agregar {
+            float: right;
+            margin-top: 20px;
+            margin-left: 10px; 
+            background-color: #4CAF50;
+        }
 
-            <div class='table-container'>
-                <table class='tabla'>
-                    <thead>
-                        <tr>
-                            <th>Nombre</th>
-                            <th>Descripción</th>
-                            <th>Precio</th>
-                            <th>Imagen</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><?php echo $data['Nombre_Prod']; ?></td>
-                            <td><?php echo $data['Descripcion']; ?></td>
-                            <td><?php echo $data['Precio']; echo " &#128184;"?></td>
-                            <td><img src="Vistas/img/<?php echo $data['Nombre_Prod']; ?>.png" alt="Imagen del producto"></td>
-                        </tr>
-                    </tbody>
-                </table>
-                <!--<br><br><br><br><br><br><br><br><br><br> -->
-            </div>
+        .tabla td.nombre {
+            width: 150px;
+        }
+    </style>
+</head>
+<body>
+    <div class='container'>
+        <h1 style='color:black; margin-top: 20px;'>Más información del producto seleccionado</h1>
 
-            <!-- Botón de flecha hacia atrás -->
-            <a class="back-button" href="index.php?controller=ProductController&action=getAllProducts">&#8592; Volver a todos los productos</a>
-
-            <!-- Botón "Agregar" -->
-            <a href="index.php?controller=ProductController&action=aniadirCarrito&id=<?php echo $data['ID_Producto']; ?>" class="boton-agregar">Agregar</a>
+        <div class='table-container'>
+            <table class='tabla'>
+                <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Descripción</th>
+                        <th>Precio</th>
+                        <th>Imagen</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><?= $data['Nombre_Prod'] ?></td>
+                        <td><?= $data['Descripcion'] ?></td>
+                        <td><?= $data['Precio'] . " &#128184;" ?></td>
+                        <td><img src="Vistas/img/<?= $data['Nombre_Prod'] ?>.png" alt="Imagen del producto"></td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
-        
-    </body>
+
+        <!-- Botón de flecha hacia atrás -->
+        <a class="back-button" href="index.php?controller=ProductController&action=getAllProducts">&#8592; Volver a todos los productos</a>
+
+        <!-- Botón "Agregar" -->
+        <a href="index.php?controller=ProductController&action=aniadirCarrito&id=<?= $data['ID_Producto'] ?>" class="boton-agregar">Agregar</a>
+    </div>
+</body>
 </html>
 
 <?php
