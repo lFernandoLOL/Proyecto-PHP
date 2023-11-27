0316874e9e098c3adcb5a67a5465d23f01c98327
Fernando <114144297+lFernandoLOL@users.noreply.github.com>
2023-11-27 09:33:39 +0100

    Update ProductById.php

--- Entorno Desarrollo PHP/public/Vistas/ProductById.php
+++ Entorno Desarrollo PHP/public/Vistas/ProductById.php
@@ -92,10 +92,10 @@
         <a class="back-button" href="index.php?controller=ProductController&action=getAllProducts">&#8592; Volver a todos los productos</a>
 
         <!-- Formulario para agregar al carrito -->
-        <?php if(($_SESSION['perfil']) != 1){
-        echo "<form class=\"form\" action=\"index.php?controller=ProductController&action=aniadirCarrito&id={$data['ID_Producto']}\" method=\"post\">";
+        <?php if((isset($_SESSION['perfil']) && $_SESSION['perfil']) != 1){
+        echo "<form class='form' action='index.php?controller=ProductController&action=aniadirCarrito&id={$data['ID_Producto']}' method='post'>";
         echo '<input type="number" name="cantidad" id="cantidad" value="1" min="1" max="10" class="cantidad-input">';
-            echo "<input type='submit' value='Agregar al carrito' class='boton-agregar'>";
+            echo "<input type='submit' value='Agregar al carrito' class='boton-agregar' >";
          echo "</form>";
         }
         ?>
